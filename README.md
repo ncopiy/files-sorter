19d0ab8f55b52935d98969cf7f4aaa7abcffa926
ncopiy <ncopiy@ya.ru>
2024-06-02 14:00:39 +0200

    Add logs

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 Copies files from source directories to destination directory and sorts it by timestamp
 
-Example of expected result:
+Example of the expected result:
 
 ```
 destination/
 ├─ 2017/
 │  ├─ 04/
 │  │  ├─ 05/
+│  │  │  ├─ file created 05.04.2017
 │  │  │  ├─ photo taken 05.04.2017
 ├─ 2022/
 │  ├─ 12/
 │  │  ├─ 23/
 │  │  │  ├─ photo taken 23.12.2022
 ```
+
+[//]: # (Tip for future self: to find the DCIM dir connect iphone to linux, open dir and drop the `:3` from the path. the path to the DCIM dir will be something like `/run/user/1000/gvfs/afc:host=00008101-001169342261001E/DCIM`)
+[//]: # (Tip for future self 2: drop the files from `recently deleted` folder to prevent handling)
--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from pathlib import Path
 from shutil import copy as shutil_copy
 from typing import List
 
+SOURCES = [
+    "/my/source/dir"
+]
+
+DESTINATION = "/my/destination/dir"
+
 
 def get_target_path(destination, entry) -> str:
     stat = entry.stat()
@@ -16,23 +22,48 @@ def get_target_path(destination, entry) -> str:
 def main(paths: List[str], destination: str) -> None:
     path_to_existed = {}
 
+    total = 0
+
     for i, path in enumerate(paths):
+        files_count = 0
+        dirs_count = 0
+
         for entry in os.scandir(path):
+            if entry.is_dir():
+                dirs_count += 1
+            else:
+                files_count += 1
+
+        print(f"{datetime.now()} Processing {i + 1}/{len(paths)} directory '{path}' with {files_count} files and {dirs_count} dirs")
+
+        total += files_count
+
+        for j, entry in enumerate(os.scandir(path)):
+            print(f"{j + 1}/{files_count + dirs_count}")
+
             if entry.is_dir():
                 paths.append(entry.path)
                 continue
             
             target = get_target_path(destination, entry)
+            target_with_name = os.path.join(target, entry.name)
 
             if target not in path_to_existed:
                 path_to_existed[target] = True
                 Path(target).mkdir(parents=True, exist_ok=True)
+            else:
+                if os.path.isfile(target_with_name):
+                    # already exists, skip
+                    continue
 
-            target_with_name = os.path.join(target, entry.name)
             source = os.path.join(path, entry.name)
 
             shutil_copy(source, target_with_name)
 
+    print(f"{datetime.now()} Processed {total} files")
+
 
 if __name__ == '__main__':
-    main(paths=[], destination="")
+    print(f"START {datetime.now()}")
+    main(paths=SOURCES, destination=DESTINATION)
+    print(f"END {datetime.now()}")
